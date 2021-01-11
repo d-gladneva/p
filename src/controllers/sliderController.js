eeb9c7540a06225725b6f6eecd8d1a54ad559b39
Darya Gladneva <davydova_di@mail.ru>
2021-01-11 15:05:23 +0300

    feat: added switching by arrows and dots in slider

--- src/controllers/sliderController.js
+++ src/controllers/sliderController.js
@@ -1,31 +1,87 @@
 {
-    const slider = () => {
-      const slide = document.querySelectorAll('.portfolio-item');
-      const slider = document.querySelector('.portfolio-content');
-      const btn = document.querySelectorAll('.portfolio-btn');
-      const dot = document.querySelectorAll('.dot');
+  const portfolioDots = document.querySelector('.slider-dots');
+  const slide = document.querySelectorAll('.slider-item');
+  const renderDots = (q) => {
+    const li = `
+            <li class="dot"></li>
+        `;
+    return li.repeat(q);
+  };
 
-      let currentSlide = 0;
+  portfolioDots.insertAdjacentHTML('beforeEnd', renderDots(slide.length));
 
-      const autoPlaySlide = () => {
-        slide[currentSlide].classList.remove('portfolio-item-active');
+  const slider = () => {
+    const slide = document.querySelectorAll('.slider-item');
+    const slider = document.querySelector('.slider-content');
+    const btn = document.querySelectorAll('.slider-btn');
+    const dot = document.querySelectorAll('.dot');
+
+    let currentSlide = 0;
+
+    const prevSlide = (elem, index, strClass) => {
+      elem[index].classList.remove(strClass);
+    };
+
+    const nextSlide = (elem, index, strClass) => {
+      elem[index].classList.add(strClass);
+    };
+
+    const autoPlaySlide = () => {
+      prevSlide(slide, currentSlide, 'slider-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+      currentSlide++;
+      if (currentSlide >= slide.length) {
+        currentSlide = 0;
+      }
+      nextSlide(slide, currentSlide, 'slider-item-active');
+      nextSlide(dot, currentSlide, 'dot-active');
+    };
+
+    const startSlide = (time) => {
+      setInterval(autoPlaySlide, time);
+    };
+
+    const stopSlide = () => {
+      clearInterval(interval);
+    };
+
+    slider.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      let target = event.target;
+
+      if (!target.matches('.slider-btn, .dot')) {
+        return;
+      }
+
+      prevSlide(slide, currentSlide, 'slider-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+
+      if (target.matches('#arrow-right')) {
         currentSlide++;
-        if (currentSlide>=slide.length){
+        if (currentSlide >= slide.length) {
           currentSlide = 0;
         }
-        slide[currentSlide].classList.add('portfolio-item-active');
-      };
+      } else if (target.matches('#arrow-left')) {
+        currentSlide--;
+        if (currentSlide < 0) {
+          currentSlide = slide.length - 1;
+        }
+      } else if (target.matches('.dot')) {
+        dot.forEach((elem, index) => {
+          if (elem === target) {
+            currentSlide = index;
+          }
+        });
+      }
 
-      const startSlide = () => {
-        setInterval(autoPlaySlide, 2000);
-      };
+      nextSlide(slide, currentSlide, 'slider-item-active');
+      nextSlide(dot, currentSlide, 'dot-active');
 
-      const stopSlide = () => {
-        clearInterval(interval);
-      };
+    });
 
-      startSlide();
+    startSlide(2000);
 
-    };
-    slider();
+  };
+  slider();
 }
