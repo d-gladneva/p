1971ec0adeceab79165c4f62a278eebf2e121ccc
Darya Gladneva <davydova_di@mail.ru>
2021-01-12 16:09:01 +0300

    fix: slider

--- src/controllers/burgerController.js
+++ src/controllers/burgerController.js
@@ -1,27 +1,27 @@
-const phone = () => {
-  const phoneBody = document.querySelector('#phoneBody');
-  let f = false;
-  if (document.documentElement.clientWidth < 768) {
-    document.addEventListener('click', (e) => {
-      let target = e.target;
-      if (!f) {
-        target = target.closest('.header__phone_icon');
-        if (target && target.classList.contains('header__phone_icon')) {
-          phoneBody.style.display = 'block';
-          f = true;
-        }
-      } else if (f) {
-        if (!target.classList.contains('phone__body')) {
-          phoneBody.style.display = 'none';
-          f = false;
-        }
-      }
-    });
-  }
-};
-
-phone();
-
-window.addEventListener('resize', () => {
-  phone();
-});
+// const phone = () => {
+//   const phoneBody = document.querySelector('#phoneBody');
+//   let f = false;
+//   if (document.documentElement.clientWidth < 768) {
+//     document.addEventListener('click', (e) => {
+//       let target = e.target;
+//       if (!f) {
+//         target = target.closest('.header__phone_icon');
+//         if (target && target.classList.contains('header__phone_icon')) {
+//           phoneBody.style.display = 'block';
+//           f = true;
+//         }
+//       } else if (f) {
+//         if (!target.classList.contains('phone__body')) {
+//           phoneBody.style.display = 'none';
+//           f = false;
+//         }
+//       }
+//     });
+//   }
+// };
+//
+// phone();
+//
+// window.addEventListener('resize', () => {
+//   phone();
+// });
