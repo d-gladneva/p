79c7c62e4d4465d42860f0b62e9e76331daad6ce
Darya Gladneva <davydova_di@mail.ru>
2021-05-05 13:50:50 +0300

    fix: upButton

--- src/controllers/upButtonController.js
+++ src/controllers/upButtonController.js
@@ -1,18 +1,18 @@
-const root = document.querySelector('#root');
-const upButton = document.querySelector('#upButton');
+const root = document.querySelector("#root");
+const upButton = document.querySelector("#upButton");
 
 const showUpButton = () => {
   if (root.getBoundingClientRect().y < -300) {
-    upButton.style.opacity = '1';
-  } else upButton.style.opacity = '0';
+    upButton.style.opacity = "1";
+  } else upButton.style.opacity = "0";
 };
 
 const scrollingTo = () => {
   if (root.getBoundingClientRect().y < 0) {
     window.scrollBy(0, -100);
-    setTimeout(scrollingTo, 20);
+    // setTimeout(scrollingTo, 1);
   }
 };
 
-window.addEventListener('scroll', showUpButton);
-upButton.addEventListener('click', scrollingTo);
+window.addEventListener("scroll", showUpButton);
+upButton.addEventListener("click", scrollingTo);
